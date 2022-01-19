9a41da7fe04513bb4608f547d6235bfef270e49d
alaeddine245 <alaeddinefattouch@insat.u-carthage.tn>
2022-01-19 15:01:23 +0100

    eliminate duplicate code

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ let score = 20;
 let highscore = 0;
 const scoreEl = document.querySelector(".score");
 
+const showMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
 document.querySelector(".check").addEventListener("click", () => {
   const guess = Number(document.querySelector(".guess").value);
-  const message = document.querySelector(".message");
   if (!guess) {
-    message.textContent = "No number!";
+    showMessage("No number!");
   } else if (guess === number) {
     document.querySelector(".number").textContent = number;
-    message.textContent = "Correct Number!";
+    showMessage("Correct Number!");
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
 
@@ -22,7 +25,7 @@ document.querySelector(".check").addEventListener("click", () => {
     }
   } else {
     if (score > 1) {
-      message.textContent = guess < number ? "Too low!" : "Too high!";
+      showMessage(guess < number ? "Too low!" : "Too high!");
       score--;
       scoreEl.textContent = score;
     } else {
@@ -35,7 +38,7 @@ document.querySelector(".check").addEventListener("click", () => {
 document.querySelector(".again").addEventListener("click", () => {
   number = Math.trunc(Math.random() * 20) + 1;
   score = 20;
-  document.querySelector(".message").textContent = "Start guessing...";
+  showMessage("Start guessing...");
   document.querySelector(".score").textContent = score;
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").textContent = "?";
